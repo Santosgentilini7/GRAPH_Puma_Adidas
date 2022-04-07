cc6f86d25e683f3f804806ea8cb5be17743872c3
Santos Gentilini <sgentilini.ieu2020@student.ie.edu>
2022-04-07 18:51:40 +0200

    Graphs about Adidas and Puma Stock.

--- graph.py
+++ graph.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 # pd.plotting.register_matplotlib_converters()
 
-df1 = pd.read_csv("AAPL.csv")
+df1 = pd.read_csv("ADS.DE.csv")
 print(df1.head())
 df1['Date'] = pd.to_datetime(df1.Date)
 # print(df1.head())
 
-df2 = pd.read_excel("iphone-dates-2019.xlsx")
+df2 = pd.read_csv("PUM.DE.csv")
 print(df2)
 df2['Date'] = pd.to_datetime(df2.date)
 # indexes = []
@@ -18,22 +18,27 @@ df2['Date'] = pd.to_datetime(df2.date)
 #             indexes.append(index)
 # print(indexes)
 
-index2 = []
-for date2 in df2.Date:
-    if df1.index[df1.Date == date2].values.size:
-        index2.append(int(df1.index[df1.Date == date2].values[0]))
-print(index2)
-
 
 mean = df1["Close"].mean()
 
+mean2 = df2["Close"].mean()
 
-plt.figure("Apple Stock")
-plt.plot(df1["Date"], df1["Close"], 'r-', linewidth=0.6, label="APPL Stock price, mean="+str(mean))
+plt.figure("Adidas Stock")
+plt.plot(df1["Date"], df1["Close"], 'r-', linewidth=0.6, label="Adidas Stock, mean="+str(mean))
 # or the same can be:
 # plt.plot("Date", "Close", 'r-', linewidth=0.6, label="APPL Stock price, mean="+str(mean), data=df1)
-plt.plot(df1["Date"], df1["Close"], 'o', ms=7, markevery=index2, label="Iphone launch date")
+
+plt.xlabel("Dates")
+plt.legend(loc="upper left")
+
+plt.figure("Puma Stock")
+plt.plot(df2["Date"], df2["Close"], 'r-', linewidth=0.6, label="Puma stock, mean="+str(mean2))
+# or the same can be:
+# plt.plot("Date", "Close", 'r-', linewidth=0.6, label="APPL Stock price, mean="+str(mean), data=df1)
+
 plt.xlabel("Dates")
 plt.legend(loc="upper left")
 
 plt.show()
+
+print(plt.show())
